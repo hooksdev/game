68860b812d57c4a5eef87de50226424f4babedfd
steven.hooks <steven.hooks@STEHOOKS3-NB7>
2016-10-31 21:12:11 -0400

    Initial Project commit

---
+++ BasicRpgLib/BasicRpgLib/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicRpgLib
+{
+    /// <summary>
+    /// Super class for all player objects including:
+    /// Hero, Monster and Non-combatants
+    /// </summary>
+    public class Player
+    {
+        public string Name;
+        public int HitPoints;
+        public int MagicPoints;
+        public int AttackPoints;
+        public int DefensePoints;
+        public int Level;
+        //public List<Spell> Spells;
+        
+        public int CalculateWeaponDamage()
+        {
+            //TODO: STUB
+
+            return 0;
+        }
+
+        public int CalculateMagicDamage()
+        {
+            //TODO: STUB
+
+            return 0;
+        }
+
+        //public List<Spells> GetSpells(){}
+
+    }
+}
---
+++ BasicRpgLib/BasicRpgLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BasicRpgLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MModal")]
+[assembly: AssemblyProduct("BasicRpgLib")]
+[assembly: AssemblyCopyright("Copyright © MModal 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7fb37a14-ee58-4180-9fcd-1985425cb8cf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
